4e706d7e7355302bce2f725a30516d3309e160bb
Dmitrii Borisov <dm1.br@yandex.com>
2025-08-26 21:20:16 +0300

    feat: finished module-federation example

--- microfrontends/module-federation/vite-app/vite.config.ts
+++ microfrontends/module-federation/vite-app/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { federation } from '@module-federation/vite'
 
-// https://vite.dev/config/
+const mfConfig = {
+    name: 'vite_app',
+    filename: 'remoteEntry.js',
+    exposes: {
+        './ViteModule': './src/App.tsx',
+    },
+    shared: ['react', 'react-dom'],
+}
+
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react(), federation(mfConfig)],
+    server: {
+        port: 3002,
+    },
 })
--- microfrontends/module-federation/vite-host/vite.config.ts
+++ microfrontends/module-federation/vite-host/vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { federation } from '@module-federation/vite'
+import type { ModuleFederationOptions } from '@module-federation/vite/lib/utils/normalizeModuleFederationOptions'
 
-// https://vite.dev/config/
+const mfConfig = {
+    name: 'host',
+    remotes: {
+        webpack_app: {
+            entry: 'http://localhost:3001/remoteEntry.js',
+            type: 'var',
+        },
+        vite_app: {
+            entry: 'http://localhost:3002/remoteEntry.js',
+            type: 'module',
+        },
+    },
+    shared: ['react', 'react-dom'],
+} as unknown as ModuleFederationOptions
+
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), federation({ ...mfConfig })],
     server: {
-        port: 8080,
+        host: true,
+        port: 3000,
     },
 })
